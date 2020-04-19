d1cc5d3df40d659876da88bebd85ebf3cb8b6174
adriancast <adrian.castellanos@roomdi.com>
2020-04-19 16:15:15 +0200

    Added filters to results.php

--- results.php
+++ results.php
@@ -5,4 +5,17 @@ require_once __DIR__ . '/config.php';
 $events_string = file_get_contents(__DIR__ . "/json/events.json");
 $events = json_decode($events_string, true)["eventos"];
 
-echo $twig->render('results.twig', compact('events', 'events_string'));
+$tags = [];
+foreach ($events as $event) {
+    $event_tags = $event['tags-propios'];
+    for ($x = 0; $x < sizeof($event_tags); $x++) {
+        $tag = $event_tags[$x];
+        array_push($tags, $tag);
+    }
+
+}
+$tags = array_unique($tags);
+
+
+
+echo $twig->render('results.twig', compact('events', 'tags'));
