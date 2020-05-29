3d6b52a1eb060194389f3ebf3044a614b969ba06
adriancast <adrian.castellanos@roomdi.com>
2020-05-29 22:14:21 +0200

    Added map and weather API call

--- product.php
+++ product.php
@@ -47,6 +47,31 @@ foreach ($events as $event) {
 $top_vehicles = array_merge($top_autos_p, $top_autoverde_p);
 shuffle($top_vehicles);
 
+// Weather 
+$curl = curl_init();
+$url_params = array(
+   "lat" => $selected_event['geo1']['lat'],
+   "lon"=> $selected_event['geo1']['long'],
+   "appid"=> "86c1057fe89870513bbbe6459dc3e11e",
+);
+$url = "api.openweathermap.org/data/2.5/weather?" . http_build_query($url_params);
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+));
+
+$weather = curl_exec($curl);
+
+curl_close($curl);
+//echo $weather;
+
 echo $twig->render('product.twig', compact('top_events', 'top_vehicles', 'selected_event'));
 
 //$autoverdes_string = file_get_contents(__DIR__ . "/json/AutoVerde.json");
